d781536d81044958bf6cccab933283aac8f51e50
Abdan Yt <161279209+Abdan-Shafiq@users.noreply.github.com>
2024-11-03 18:33:15 +0500

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-KhpsLfd-HVEaa8UuZ9oiKXTWO3Pb9sE",
-  authDomain: "resturent-web.firebaseapp.com",
-  projectId: "resturent-web",
-  storageBucket: "resturent-web.firebasestorage.app",
-  messagingSenderId: "626946658563",
-  appId: "1:626946658563:web:be2ddd2ff83f6350c05936"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
